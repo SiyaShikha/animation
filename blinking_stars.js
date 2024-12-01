83b626833174c1badf1236086f8906c38109ef1c
Shikha Singh <150659967+SiyaShikha@users.noreply.github.com>
2024-12-01 20:13:32 +0530

    Create blinking_stars.js

---
+++ blinking_stars.js
@@ -0,0 +1,46 @@
+const LENGTH = 30;
+const WIDTH = 20;
+
+function repeat(string, nTimes) {
+  let repeatedString = "";
+
+  for (let index = 0; index < nTimes; index++) {
+    repeatedString += string;
+  }
+
+  return repeatedString;
+}
+
+function generateStar(to, from) {
+  const star = to + Math.ceil(Math.random() * (from - to));
+  return star;
+}
+
+function createSky(length, width) {
+  let sky = "";
+
+  for (let row = 1; row <= width; row++) {
+    const star1 = generateStar(1, length / 2);
+    const star2 = generateStar(length / 2, length);
+
+    sky += "\n" + repeat(" ", star1 - 1) + "✨";
+    sky += repeat(" ", (star2 - star1 - 1)) + "✨";
+    sky += repeat(" ", length - (star2));
+  }
+
+  return sky;
+}
+
+function delay(time) {
+  for (let i = 0; i <= time; i++) { }
+}
+
+function blinkStars(n) {
+  for (let sky = 1; sky <= n; sky++) {
+    console.log(createSky(LENGTH, WIDTH));
+    delay(800000000);
+    console.clear();
+  }
+}
+
+blinkStars(10);
